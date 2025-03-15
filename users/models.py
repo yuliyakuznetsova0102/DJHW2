cfe53017eef028f882bad057d7249eb65c9bdb9d
Yuliya Kuznetsova <juliakuznetsova001@gmail.com>
2025-03-15 14:19:11 +0300

    com1

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15, blank=True, null=True)
