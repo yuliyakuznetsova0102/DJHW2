d1f74604a013c746d0b920d0e0621a3f91c235ac
Yuliya Kuznetsova <juliakuznetsova001@gmail.com>
2025-05-13 20:57:39 +0300

    hw 35.2 ci.yaml

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from dotenv import load_dotenv
 from pathlib import Path
-from urllib.parse import urlparse
 
 
 load_dotenv(override=True)
